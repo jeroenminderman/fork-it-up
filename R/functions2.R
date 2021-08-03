f9ebde0e7a85be5746502bbabae6dcbf1fd7182a
Your Name <you@example.com>
2021-08-03 15:54:30 +1100

    Tourism data processing

---
+++ R/functions2.R
@@ -0,0 +1,37 @@
+# Getting started with functions
+
+values<- c(2,4,5,6,7,8,9,1,3,5)
+
+#mean(values)
+length_of_values <- length(values)
+sum_of_values <- sum(values)
+mean_of_values <- sum_of_values / length_of_values
+
+second_Set_of_Values <- c(2,4,3,5,6,9,0,1,7,8)
+length_of_values <- length(second_Set_of_Values)
+sum_of_values <- sum(second_Set_of_Values)
+mean_of_second_set_of_Values <- sum_of_values / length_of_values
+
+# count the number of elements in input vector
+length_of_vector <- length(numeric_Vectors)
+
+calculate_mean <- function(numeric_vector){return(mean_of_vector)}
+ 
+#General function structure that we use:
+
+function_name <- function(input){...}
+
+
+# Given a vector of numeric values this function will calculate the mean
+# @param numeric_vector A numeric vector of numbers
+# @return Returns a single numeric value
+# @examples - Give an example of what the code would look like
+
+
+
+function_name <-function(first, second, third,..)
+
+
+
+
+
