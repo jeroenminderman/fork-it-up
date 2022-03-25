d7e97ab837cae6753a0ce97e4d528bf13655725b
Your Name <you@example.com>
2022-03-25 19:58:00 +1100

    Tab 7

--- R/MonthlyReportAndTables/1_processLatestMonthsTourismStats.R
+++ R/MonthlyReportAndTables/1_processLatestMonthsTourismStats.R
@@ -7,6 +7,7 @@ rm(list = ls())
 #install.packages("dplyr")
 library(dplyr) # Manipulating data
 library(openxlsx) # Reading and editing excel formatted files
+library(janitor)
 
 # Note where VNSO code/data is on current computer
 repository <- file.path(dirname(rstudioapi::getSourceEditorContext()$path), "..", "..")
@@ -120,6 +121,13 @@ Tab2_POV <- TourismFINAL %>%
 write.csv(Tab2_POV, file = "Tab 2_Purpose of Visit")
 
 #### Table 3: Country of Usual Residence ####
+Tab3_VistorsCUR <- TourismFINAL %>%
+  filter(PORT %in% c("SAIR","SAIRP","VAIR","VAIRP"))%>%
+  filter(VisitorResident %in%("Visitor"))%>%
+  filter(ARR.DEPART %in%("ARRIVAL"))%>%
+  group_by(GROUP) %>%
+  count()
+
 
 #### Table 4: Residents arrival by Nationality ####
 Tab4_ResByNat<- TourismFINAL %>%
@@ -171,6 +179,34 @@ Tab5_AVGLOS <- rbind(AVG_LOS_VUV,Vanuatu)
 
 
 #### Table 6: Average Age ####
+str(TourismFINAL$AGE)
+str(TourismFINAL$VisitorResident)
+
+visitor_data_age <- TourismFINAL %>%
+  filter(VisitorResident == "Visitor")
+
+
+visitor_data_age <- visitor_data_age %>%
+  mutate(cities = case_when(
+    grepl(pattern = "VAI", visitor_data_age$PORT) ~ "PORT VILA",
+    grepl(pattern = "SAI", visitor_data_age$PORT) ~ "LUGANVILLE"
+  )) 
+
+
+AVG_AGE_VUV <- visitor_data_age %>%
+  filter(ARR.DEPART == "ARRIVAL") %>%
+  filter(AGE != "NA") %>%
+  filter(cities %in% c("PORT VILA", "LUGANVILLE")) %>%
+  group_by(cities) %>%
+  summarise(AVG_AGE = round(mean(AGE), digits = 0)) 
+
+Vanuatu_AGE <- mean(AVG_AGE_VUV$AVG_AGE)
+
+cities <- "VANUATU"
+AVG_AGE <- 38.5
+Vanuatu <- data.frame(cities, AVG_AGE)
+
+Tab6_AVGAGE <- rbind(AVG_AGE_VUV,Vanuatu)
 
 #### Table 7: Visitors to outer islands ####
 
@@ -186,6 +222,102 @@ Tab7_VisitorOuterIsland <- FINALOuterIsland %>%
   filter(PORT %in% c("VAIRP","VAIR","SAIRP","SAIR")) %>%
   count()
 
+#### Table 8: Proportion of visitors by country of usual residence and purpose of visit ####
+#grouping the purpose of visits
+POV_Tab8 <- TourismFINAL %>%
+  mutate( POV_Groups = case_when(
+    grepl(pattern = "Bus", TourismFINAL$TravelPurposeClassifications) ~ "Business",
+    grepl(pattern = "Conf", TourismFINAL$TravelPurposeClassifications) ~ "Business",
+    grepl(pattern = "Edu", TourismFINAL$TravelPurposeClassifications) ~ "All Other",
+    grepl(pattern = "Oth", TourismFINAL$TravelPurposeClassifications) ~ "All Other",
+    grepl(pattern = "Spo", TourismFINAL$TravelPurposeClassifications) ~ "All Other",
+    grepl(pattern = "Sto", TourismFINAL$TravelPurposeClassifications) ~ "All Other",
+    grepl(pattern = "Hol", TourismFINAL$TravelPurposeClassifications) ~ "Holiday",
+    grepl(pattern = "Vis", TourismFINAL$TravelPurposeClassifications) ~ "Visitings Friends and relatives"
+  )) 
 
+
+#creating the data frame comparing visitors country of usual residence by purpose of visit
+UsualResByPOV <- POV_Tab8 %>%
+  group_by(GROUP, POV_Groups) %>%
+  filter(VisitorResident %in% c("Visitor")) %>%
+  filter (ARR.DEPART %in% c("ARRIVAL")) %>%
+  count()
+
+
+#manipulating the above to a wider format
+UsualResByPOV2 <- UsualResByPOV %>%
+  pivot_wider (names_from = POV_Groups, values_from = n)
+
+### Add in total row and column ###
+#adding a total column
+
+UsualResByPOV2 <- UsualResByPOV2 %>%
+  adorn_totals("col")
+
+#adding a total row
+UsualResByPOV2 <- UsualResByPOV2 %>%
+  adorn_totals("row")
+
+##Percentage of Total Columns
+
+#Calculate Percentage of total
+n_cols <- ncol(UsualResByPOV2)
+columns_of_interest <- 2:(n_cols - 1) #ignores the first and last columns
+UsualResByPOV_proportion <- UsualResByPOV2[, columns_of_interest]/UsualResByPOV2$Total
+UsualResByPOV_percentage <- UsualResByPOV_proportion * 100
+
+#Note percentage columns in their name
+colnames(UsualResByPOV_percentage) <- paste(colnames(UsualResByPOV_percentage),"(% share)")
+
+#Add percentage columns into main table
+UsualResByPOV_percentage <- cbind(UsualResByPOV2, UsualResByPOV_percentage)
+
+##Combining Value and Percentage into one single column
+
+#Initialise a dataframe to store the combined values and percentages
+Tab8_UsualResByPOV <- data.frame("GROUP" = UsualResByPOV2$GROUP)
+
+#Note columns of interest
+columns_of_interest <- colnames(UsualResByPOV2)[
+  c(-1, -ncol(UsualResByPOV2))
+]
+
+#Add in each column with combined value and percentages
+for(travel_type_column in columns_of_interest){
+  
+  #create column name with percentage
+  travel_type_column_as_percentage <- paste(travel_type_column, "(% share)")
+  
+  #get the counts for current travel type
+  #Note use of "drop" - this extracts the values as a vector instead of a data.frame
+  
+  travel_type_counts <- UsualResByPOV2 [, travel_type_column, drop = TRUE]
+  
+  #get the counts as percentages
+  #Note use of "drop" - this extracts the values as a vector instead of a data.frame
+  
+  travel_type_percentages <- UsualResByPOV_percentage [,travel_type_column_as_percentage, drop = TRUE]
+  
+  #Round as percentages
+  travel_type_percentages <- round(travel_type_percentages, digits = 0)
+  
+  #create column with combined value and percentage
+  Tab8_UsualResByPOV[, travel_type_column_as_percentage] <- paste(
+    travel_type_counts, "(", travel_type_percentages, "%)", sep = ""
+  )
+}
+
+#Add a total column
+Tab8_UsualResByPOV$Total <- UsualResByPOV2$Total
+
+#Remove the percentages from final row
+
+number_rows <- nrow(Tab8_UsualResByPOV)
+Tab8_UsualResByPOV[number_rows,] <- UsualResByPOV2[number_rows, ]
+Tab8_UsualResByPOV <- Tab8_UsualResByPOV[ , c(1,5, 4, 3, 2, 6)]
+
+
+write.csv(TourismFINAL, 'TourismFINAL.csv')
 
 
